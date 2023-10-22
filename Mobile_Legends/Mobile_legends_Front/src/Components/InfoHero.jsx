f7489a8652f93876fccab723622eac814dcd3730
cass <alicastilo1097@gmail.com>
2023-10-22 06:03:38 +0300

    add Spell

--- Mobile_Legends/Mobile_legends_Front/src/Components/InfoHero.jsx
+++ Mobile_Legends/Mobile_legends_Front/src/Components/InfoHero.jsx
@@ -7,16 +7,35 @@ import { useSelector } from "react-redux";
 function InfoHero() {
   const { showHero } = useSelector((state) => state.heros);
 
+  const skins =
+    showHero.skins.length &&
+    showHero.skins.map((ele, i) => (
+      <div  key={i}>
+        <img src={ele.path} alt="" />
+      </div>
+    ));
 
-const skins =
-  showHero.skins.length &&
-  showHero.skins.map((ele, i) => (
+  const build =
+    showHero.build.length &&
+    showHero.build.map((ele, i) => (
+      <div key={i} className="">
+        <img src={ele.path} alt="" />
+        {/* <span className="text-white text-xs">{ele.name}</span> */}
+      </div>
+    ));
 
-    <div  key={i}>
-      <img src={ele.path} alt="" />
-    </div>
-  ));
-
+  const spell =
+    showHero.recommended_spell.length &&
+    showHero.recommended_spell.map((ele, i) => (
+      <div key={i} className="text-white">
+        <ul>
+          <li className="flex items-center gap-3">
+            <img src={ele.path} alt="" />
+            <span className="text-2xl">{ele.name}</span>
+          </li>
+        </ul>
+      </div>
+    ));
 
   return (
     <div>
@@ -31,20 +50,69 @@ const skins =
               <h1>
                 {showHero.name} ({showHero.role})
               </h1>
-              <h3>
+              <h3 className="mt-5">
                 Release Date: <span> {showHero.release_date} </span>
               </h3>
-              <h3>
+              <h3 className="mt-1">
                 Lane Recc: <span> {showHero.lane_recc}</span>
               </h3>
-              <h3>
+              <h3 className="mt-1">
                 Price: <span> {showHero.price} </span>
               </h3>
-              <h3>
-                Damage Type: <span> {showHero.damage_type} </span>
+              <h3 className="mt-1">
+                Damage Type: <span> {showHero.damage_type} </span>+
               </h3>
+              <div className="hold_data">
+                <div className="label">Durability</div>
+                <div className="hold_sled">
+                  <div
+                    style={{
+                      width: showHero.durability,
+                      background: "#fffb00",
+                    }}
+                    className="slid "
+                  ></div>
+                </div>
+                <div className="label">Offense</div>
+                <div className="hold_sled">
+                  <div
+                    style={{ width: showHero.offense, background: "#ffee00" }}
+                    className="slid "
+                  ></div>
+                </div>
+                <div className="label">Ability Effects</div>
+                <div className="hold_sled">
+                  <div
+                    style={{
+                      width: showHero.ability_effects,
+                      background: "#00d5ff",
+                    }}
+                    className="slid "
+                  ></div>
+                </div>
+                <div className="label">Difficulty</div>
+                <div className="hold_sled">
+                  <div
+                    style={{
+                      width: showHero.difficulty,
+                      background: "#00ff4c",
+                    }}
+                    className="slid "
+                  ></div>
+                </div>
+              </div>
             </div>
-            <div className="imp"></div>
+            <div className="hold_build">{/* <div>{build}</div> */}</div>
+            <div></div>
+            <div className="build">
+              <h2 className="head">Default Build</h2>
+              <div className="flex flex-row	justify-around">{build}</div>
+            </div>
+            <div className="imp">
+              {/* <h2 className="head">Recommended Spell</h2> */}
+              <h2 className="head">Spell</h2>
+              <div className="mt-3">{spell}</div>
+            </div>
           </>
         )}
       </div>
